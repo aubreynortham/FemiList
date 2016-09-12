04d8e54450eb3f24244f14d64e1021d1b12d6acc
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-11 22:10:16 -0400

    changed home route name

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1><a href="/">IMDB<span id="ladyFont"> for the ladies</span></a></h1>
+        <h1><a href="/FemiList">The FemiList</a></h1>
         <SearchContainer />
       </div>
     );
