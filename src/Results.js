770f06f441633ab0b739355106506e733f309fc6
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-15 11:44:33 -0400

    Trying to add extra properties to response

--- src/Results.js
+++ src/Results.js
@@ -6,24 +6,34 @@ class Results extends Component {
   render(){
     let {movies} = this.props;
     let results = movies.map( (movie, index) => {
-      let title = $('<p>').html(movie.title).text();
-      if (title.endsWith(", The")) {
+      console.log("HEY", movie);
+      console.log("works: ", movie.title);
+      console.log("huh?: ", movie.Poster);
+
+      let title = $('<p>').html(movie.title).text(); //check for HTML code entities in title and convert to plain text
+      if (title.endsWith(", The")) { //check for and correct title structure
         title = "The " + title.substring(0, title.length-5)
       }
 
       return (
         <div key={index}>
-          <h1>{title}</h1>
-          <img src={movie.Poster} alt={title} />
-          <p>Released in {movie.year}</p>
-          <p>Bechdel Score: {movie.rating}</p>
+          <div id="movieWrap">
+            <h1>{title}</h1>
+            <img id="movPic" src={movie.Poster} alt={title} />
+            <p>Genre: {movie.Genre}</p>
+            <p>Released in {movie.year}</p>
+            <p>Parental Rating: {movie.Rated}</p>
+            <p>IMDB rating: {movie.imdbRating}</p>
+            <p>Plot: {movie.Plot}</p>
+            <h3>Bechdel Score: {movie.rating} / 3</h3>
+          </div>
         </div>
-      )
-    })
+      );
+    });
     return (
       <div>{results}</div>
-    )
+    );
   }
 }
 
-export default Results
+export default Results;
--- src/Utils.js
+++ src/Utils.js
@@ -2,20 +2,46 @@ import $ from 'jquery'
 
 export function queryOmdb (query) {
   var term = query.replace(/\s/, "+"); //replace white space characters with a "+"
-  var url = "https://omdbapi.com?s=" + term;
+  var url = "https://omdbapi.com?i="
   var bechurl = "https://cors-anywhere.herokuapp.com/http://bechdeltest.com/api/v1/getMoviesByTitle?title=" + term; //use this sorcery to
   return $.getJSON(bechurl).then(function(response){
-    return $.getJSON(url).then(function(response2){
-      for (var i = 0; i < response.length; i++) { //loop first through bech responses
-        for(var j = 0; j < response2["Search"].length; j++) { // then loop through the omdb responses
-          //check for matching imdb ids (compare omdb responses to bech responses)
-          if ("tt" + response[i]["imdbid"] === response2["Search"][j]["imdbID"]) {
-            response[i].Poster = response2["Search"][j].Poster
-          }
-        }
-      }
-      return response; //return our Bech responses with poster property we just pulled in
-    })
+      for (var i = 0; i < response.length; i++) { //loop through bech results
+        let n = i; //define this variable to use block scope
+        var bechId = "tt" + response[n]["imdbid"]; //get individual id
+        console.log(url + bechId);
+        //query individual id
+        $.getJSON(url + bechId).then(function(response2){
+          //set properties on response i.e.(Poster,genre,rated,plot)
 
+          response[n].Poster = response2.Poster;
+          response[n].Genre = response2.Genre;
+          response[n].imdbRating = response2.imdbRating;
+          response[n].Rated = response2.Rated;
+          response[n].Plot = response2.Plot;
+          //console.log("Poo", response[n]);
+          //console.log(response2);
+        })
+      }
+
+    // return $.getJSON(url).then(function(response2){
+    //   console.log("Poop",response2);
+    //   for (var i = 0; i < response.length; i++) { //loop first through bech responses
+    //     for(var j = 0; j < response2.length; j++) { // then loop through the omdb responses
+    //       //check for matching imdb ids (compare omdb responses to bech responses)
+    //       if ("tt" + response[i]["imdbid"] === response2[j]["imdbID"]) {
+    //         console.log("Match",response2[j]);
+    //         response[i].Poster = response2[j].Poster;
+    //         response[i].Genre = response2[j].Genre;
+    //         response[i].imdbRating = response2[j].imdbRating;
+    //         response[i].Rated = response2[j].Rated;
+    //         response[i].Plot = response2[j].Plot;
+    //       }
+    //     }
+    //   }
+    //   console.log(response);
+    //   return response; //return our Bech responses with poster property we just pulled in
+    // })
+    console.log(response);
+    return response
   });
 }
